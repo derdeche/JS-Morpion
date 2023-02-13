3b6f9b28c83012e3fbefc951e7e4f711f5dcb202
derdeche <foued.derdeche@gmail.com>
2023-02-13 16:37:20 +0100

    rajout function checkWin,checkDraw

--- script.js
+++ script.js
@@ -4,16 +4,51 @@ const endGameStatus = document.getElementById('endGameStatus');
 const playerOne = 'X';const playerTwo= '0';
 let playerTurn = playerOne;
 
+const winningPatterns = 
+[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 cells.forEach(cell =>{
 cell.addEventListener('click' , playGame, {once:true});
 });
 
-function playGame(e)
-{
+function playGame(e) {
     e.target.innerHTML = playerTurn;
+  
+    if (checkWin(playerTurn)) {
+      updateGameStatus("wins" + playerTurn);
+      return endGame();
+    } else if (checkDraw()) {
+      updateGameStatus("draw");
+      return endGame();
+    }
+  
     updateGameStatus(playerTurn);
     playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
-}
+  }
+  
+  function checkWin(playerTurn) {
+    return winningPatterns.some(combination => {
+      return combination.every(index => {
+        return cells[index].innerHTML == playerTurn;
+      });
+    });
+  }
+  
+  function checkDraw() {
+    return [...cells].every(cell => {
+      return cell.innerHTML == playerOne || cell.innerHTML == playerTwo;
+    });
+  }
+
 
 function updateGameStatus(status)
 {
@@ -42,3 +77,6 @@ function updateGameStatus(status)
     endGameStatus.innerHTML = statusText;
 
 }
+
+function endGame() { document.getElementById('gameEnd').style.display = "block" }
+function reloadGame() { window.location.reload() }
